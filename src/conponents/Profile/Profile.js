8e1c230e0f70c437e7734998ac433d62610b31bf
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-13 16:10:28 +0300

    fix styles to App & Profile

--- src/conponents/Profile/Profile.js
+++ src/conponents/Profile/Profile.js
@@ -1,53 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
 
-const Profile = ({name, tag, location, avatar, stats}) => {
-    return (
-<div className="profile">
-  <div className="description">
-    <img
-      src={avatar}
-      alt="Аватар пользователя"
-      className="avatar"
-    />
-    <p className="name">{name}</p>
-    <p className="tag">{tag}</p>
-    <p className="location">{location}</p>
-  </div>
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img
+          src={avatar}
+          alt={'Аватар пользователя' + name}
+          className={styles.avatar}
+        />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
 
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{stats.followers}</span>
-    </li>
-    <li>
-      <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
-    </li>
-    <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
-    </li>
-  </ul>
-</div>)
-}
-
+      <ul className={styles.stats}>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 Profile.defaultProps = {
-    avatar:"https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg",
-}
+  avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+};
 
 Profile.propTypes = {
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string,
-    stats: PropTypes.shape({
-        followers:PropTypes.number.isRequired,
-        views:PropTypes.number.isRequired,
-        likes:PropTypes.number.isRequired,
-    })
-    
-}
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
 
 export default Profile;
